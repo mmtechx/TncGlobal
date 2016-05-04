990f06a8d3a97f42a0370b4c5f49f60f6fe60fec
ghulamustafa@gmail.com <ghulamustafa@gmail.com>
2016-05-04 21:50:53 +1000

    Footer fix

--- Site/footer.php
+++ Site/footer.php
@@ -1,10 +1,10 @@
     <footer id="footer" class="midnight-blue">
         <div class="container">
             <div class="row">
-                <div class="col-sm-5">
-                    &copy; 2016 <a target="_blank" href="http://www.mmtechnologies.com.au/" title="Free Twitter Bootstrap WordPress Themes and HTML templates">MM Technologies Pty Ltd</a>. All Rights Reserved.
+                <div class="col-sm-4">
+                    &copy; 2016 <a href="http://www.tncglobal.com.au/" title="TNC Global Pty Ltd">TNC Global Pty Ltd</a>. All Rights Reserved.
                 </div>
-                <div class="col-sm-7">
+                <div class="col-sm-8">
                     <ul class="pull-right">
                         <li class="active"><a href="index.php">Home</a></li>
                         <li><a href="about-us.php">About Us</a></li>
@@ -14,6 +14,9 @@
                         <li><a href="contact-us.php">Contact Us</a></li>
                     </ul>
                 </div>
+            </div>
+            <div class="row">
+                <div class="designer col-sm-offset-5">Developed by: <a target="_blank" href="http://www.mmtechnologies.com.au/" title="Free Twitter Bootstrap WordPress Themes and HTML templates">MM Technologies Pty Ltd</a></div>
             </div>
         </div>
     </footer><!--/#footer-->
